5d53d234e8ac337ff1bf3f4ea2f0bb06759cad55
tolukusan <ise.fola10@gmail.com>
2024-01-22 06:04:24 +0100

    restarted the project added image files made a loin and sign up page intergrated clerk

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+ 
+export default function Page() {
+  return (
+  <div>
+      <Image src="public/logo-white.png" width={900} height={1000}
+      className="object-contain h-full w-full"/>
+      <div className="absolute top-20 right-0">
+        
+        <SignIn />
+      </div>
+  </div>
+  );
+}
