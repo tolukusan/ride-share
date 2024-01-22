d1d3e4b1754d67476987019a1842448dbfee1f8c
tolukusan <ise.fola10@gmail.com>
2024-01-22 09:51:41 +0100

    Completed the Search Section UI

--- components/Home/InputItem.js
+++ components/Home/InputItem.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 
-function InputItem() {
+function InputItem({type}) {
   return (
-    <div className='bg-slate-200 p-3  rounded-lg mt-3'>
-        <Image src='/source.png' width={15} height={15}/>
-        <input type='text' placeholder='Pickup Location'/>
+    <div className='bg-slate-200 p-3  rounded-lg mt-3 flex items-center gap-4'>
+        <Image src={type=='source'?'/source.png':'/dest.png'} width={15} height={15}/>
+        <input type='text' placeholder={type=='source'?'Pickup Location':'Destination'} className='bg-transparent w-full outline-none'/>
     </div>
   )
 }
--- components/Home/SearchSection.js
+++ components/Home/SearchSection.js
@@ -3,13 +3,17 @@ import InputItem from './InputItem'
 
 function SearchSection() {
   return (
-    <div className='p-2 md:pd-5 
+    <div className='p-2 md:p-5 
     border-[2px] rounded-xl'>
         <p className='text-[20px] font-bold'>
             Get a ride
-            <InputItem/>
-            <InputItem/>
         </p>
+            <InputItem type='source'/>
+            <InputItem type='destination'/>
+
+            <button className='p-3 bg-black w-full mt-5 text-white rounded-lg'>
+              Search
+            </button>
     </div>
   )
 }
